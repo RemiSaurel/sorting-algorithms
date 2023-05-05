7750454b08643b323b237e00c8378dfb1c4f965c
RemiSaurel <r.saurel5@gmail.com>
2023-05-05 17:51:49 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ElementGrid from "./components/ElementGrid";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+          <h1>Sorting algorithms</h1>
+        <ElementGrid
+            nbItems={10}
+        />
+      </div>
   );
 }
 
