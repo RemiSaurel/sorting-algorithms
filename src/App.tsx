19f2f6dfd7883e0ac69b616ad89c58951bd51b78
RemiSaurel <r.saurel5@gmail.com>
2023-05-08 09:21:32 +0200

    add: absolute instead of fixed

--- src/App.tsx
+++ src/App.tsx
@@ -9,17 +9,17 @@ function App() {
           <div className="flex justify-center">
               <h1 className="text-3xl mt-4 mb-6">Tri visualizer</h1>
           </div>
+            <div className="hidden lg:flex absolute w-52 justify-start text-xl text-gray-700 top-4 left-12">
+                Légende
+            </div>
+          <div className="hidden lg:flex absolute w-52 justify-end text-xl text-blue-900 top-4 right-12 text-end">
+              Infos
+          </div>
           <div className="flex justify-center mb-4 gap-4">
-              <div className="hidden lg:flex w-52 justify-center text-xl text-gray-700 z-10 lg:fixed top-4 -left-5">
-                  Légende
-              </div>
-              <div className="flex w-52 justify-center lg:fixed top-12 left-12 border-4 border-dashed border-gray-400 bg-gray-100 rounded p-4">
+              <div className="flex w-52 justify-center lg:absolute top-12 left-12 border-4 border-dashed border-gray-400 bg-gray-100 rounded p-4">
                   <DataLegend/>
               </div>
-              <div className="hidden lg:flex w-52 justify-center text-xl text-blue-900 z-10 lg:fixed top-4 -right-8">
-                  Infos
-              </div>
-              <div className="flex w-52 justify-center lg:fixed top-12 right-12 border-4 border-dashed border-blue-900 bg-gray-100 rounded p-2">
+              <div className="flex w-52 justify-center lg:absolute top-12 right-12 border-4 border-dashed border-blue-900 bg-gray-100 rounded p-2">
                   <MiscInfos/>
               </div>
           </div>
